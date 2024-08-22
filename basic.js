c58e9827060fc08dbadc09b3f790cc3d0e2cb2ef
Adib Bin Billal <mdadib550@gmail.com>
2024-08-22 18:10:23 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,17 @@
+var a = 5;
+let b = 6;
+let c = "ADIB"
+let _d = "adib-the-noob"
+
+// console.log(a + b);
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof _d);
+
+
+{
+    var a = 88; // let is block scoped!
+    console.log(a);
+}
+console.log(a);
---
+++ dateTypes.js
@@ -0,0 +1,32 @@
+let x = "CodeRADIB";
+let y = 22;
+let z = 3.44;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+console.log("----------------");
+
+// type of null is object
+
+let o = {
+    "name": "Adib",
+    "age": 22,
+    "job_role": "Software Engineer",
+    "is_handsome": true,
+}
+console.log(o);
+
+
+o.salary="100000";
+console.log(o);
+console.log("----------------");
+
+o.salary="500000";
+console.log(o);
+
+console.log("----------------");
+
